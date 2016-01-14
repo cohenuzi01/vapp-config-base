4c506917550f1f26e42702894c0d8c67f431a194
cohenuzi01 <cohenuzi01@gmail.com>
2016-01-14 17:00:10 -0500

    merge master ps changes from dev. fix NodePort support for the policy-server service

--- base/admin-ui/init.d/03_ps_register.sh
+++ base/admin-ui/init.d/03_ps_register.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
+# 03_ps_register - This script registers the Admin UI against the Policy Server. 
+
+##### Constants
 PROP_FILE=/solution/$CONFIG/data/configuration.properties
+JBOSS_AVAILABILITY_TIME_OUT=500
+POLICYSERVER_AVAILABILITY_TIME_OUT=1000
+ADMINUI_RESIGTRATION_TIME_OUT=1000
+LOGFILE=${JBOSS_HOME}/bin/nohup.out
+JBOSS_STARTED_STRING="CA IAM FW Startup Sequence Complete"
 
 ps_host=`/opt/util/parser.sh PS_HOST $PROP_FILE`
 ps_user=`/opt/util/parser.sh PS_USERNAME $PROP_FILE`
@@ -8,66 +16,87 @@ ps_password=`/opt/util/parser.sh PS_PASSWORD $PROP_FILE`
 
 ps_password=`/solution/$CONFIG/../common/password-util/passwordDecode.sh $ps_password`
 
-adminui_registration() {
-	echo "Running admin ui registration command"
 
-	TIME_OUT=1000
-	i=0
-	echo  "Logging in to WAMUI"
-	TYPE="Content-Type: application/x-www-form-urlencoded"
-	ACCEPT="Accept: text/html"
-	
-	while true; do
-        if [ "$i" -gt "$TIME_OUT" ]; then
-            echo "Timeout reached attempting to register admin ui"
+##### Functions
+waiting_for_policyserver() {
+
+    echo "[*][$(date +"%T")] - Checking if the policy server is up and running..."
+    STARTTIME=$(date +%s)
+    # Waiting for the Policy Server service to be ready
+    while true; do
+        if [ "$(($CURRTIME - $STARTTIME))" -gt "$POLICYSERVER_AVAILABILITY_TIME_OUT" ]; then
+            echo "[*][$(date +"%T")] - A timeout was reached ($POLICYSERVER_AVAILABILITY_TIME_OUT seconds) while waiting for the policy server to be available."
             exit 1
         fi
-        
-		/usr/bin/curl -s -H "Host: admin-ui" -H "$ACCEPT" -H "$TYPE" -X POST "admin-ui/iam/siteminder/adminui" -d "username=$ps_user&password=$ps_password&address=$ps_host" | grep -q "Error"
-		if [ $? -eq 0 ]; then
-		  echo "unable to register adminui...."
-			i=`expr $i + 10`
-			echo "Sleeping for 10 secs and re trying.........."
-			echo $i
-			sleep 10
-		else
-			echo "adminui registration successful"
-			echo $i
-			retval=0
-			break
-		fi
-	done
 
-	echo "Finished running admin ui registration command.."
+        cat < /dev/tcp/policy-server-master/44443| grep -q "Connection timed out" #If not available will timeout with: bash: connect: Connection timed out.  "curl http://policy-server-master:44443" can also be used.
+                
+        if [ $? -ne 0 ]; then
+            echo "[*][$(date +"%T")] - Policy server is up and running."
+            sleep 2 #let's wait a little bit longer, just in case.
+            retval=0
+            break
+        else
+            echo "[*][$(date +"%T")] - Unable to connect to the policy server. Waiting for the policy-server to be available ($(($CURRTIME - $STARTTIME))).........."
+            sleep 10
+            CURRTIME=$(date +%s)            
+        fi
+    done
 }
 
- TIME_OUT=500
- j=0
- LOGFILE=${JBOSS_HOME}/bin/nohup.out
+adminui_registration() {
+    echo "[*][$(date +"%T")] - Running Admin UI registration command"
 
-	while [ -f "$LOGFILE" ]; do
-        if [ "$j" -gt "$TIME_OUT" ]; then
-            echo "Timeout reached waiting for jboss to start"
+    STARTTIME=$(date +%s)
+    TYPE="Content-Type: application/x-www-form-urlencoded"
+    ACCEPT="Accept: text/html"
+    
+    while true; do
+        if [ "$(($CURRTIME - $STARTTIME))" -gt "$ADMINUI_RESIGTRATION_TIME_OUT" ]; then
+            echo "[*][$(date +"%T")] - A timeout was reached ($ADMINUI_RESIGTRATION_TIME_OUT seconds) while attempting to register Admin UI."
             exit 1
         fi
         
-		/bin/grep "CA IAM FW Startup Sequence Complete" "$LOGFILE"
-		if [ $? -eq 0 ]; then
-			 echo "jboss started successfully"
-			 retval=0
-			 break
-		else
-			echo "Jboss haven't started yet"
-			 j=`expr $j + 10`
-			 echo "Sleeping for 10 secs and checking for jboss.........."
-			 sleep 10 
-		fi
-	done
+        /usr/bin/curl -s -H "Host: admin-ui" -H "$ACCEPT" -H "$TYPE" -X POST "admin-ui/iam/siteminder/adminui" -d "username=$ps_user&password=$ps_password&address=$ps_host" | grep -q "Error"
+        if [ $? -eq 0 ]; then
+            echo "[*][$(date +"%T")] - Unable to register Admin UI. Will try again in 10 seconds(($(($CURRTIME - $STARTTIME)))).........."
+            sleep 10
+            CURRTIME=$(date +%s)            
+        else
+            echo "[*][$(date +"%T")] - AdminUI registration completed successfully."
+            retval=0
+            break
+        fi
+    done
 
-adminui_registration 
+}
 
+waiting_for_jboss()
+{
+    j=0
+    while [ -f "$LOGFILE" ]; do
+        if [ "$j" -gt "$JBOSS_AVAILABILITY_TIME_OUT" ]; then
+            echo "[*][$(date +"%T")] - A timeout was reached ($POLICYSERVER_AVAILABILITY_TIME_OUT seconds) while waiting for the JBoss to start."
+            exit 1
+        fi
+        /bin/grep "$JBOSS_STARTED_STRING" "$LOGFILE"
+        if [ $? -eq 0 ]; then
+             echo "[*][$(date +"%T")] - JBoss started successfully."
+             retval=0
+             break
+        else
+            echo "[*][$(date +"%T")] - JBoss did not start yet. Waiting for JBoss to start ($j).........."
+             j=`expr $j + 10`
+             sleep 10
+        fi
+    done
+}
+
+##### Main
+waiting_for_jboss
+
+waiting_for_policyserver
+
+adminui_registration
 
 tail -f /dev/null
-
-
-
--- base/policy-server/init.d/03_set_registry_entries.sh
+++ base/policy-server/init.d/03_set_registry_entries.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-echo "[*] Create default objects: starting"
+# 03_set_registry_entries - This script initializes the Policy Server configuration files. 
+# If this script runs on a master policy server, it will initialize the policy store (in case it's not already initialized).  
+# If this script runs on a worker policy server, it will wait for the  policy store to be initialized before starting. 
 
+##### Constants
+POLICY_STORE_VERSION="r12.52 sp1"
+POLICY_STORE_VERIFICATION_ATTRIBUTE=CA.SM::AuthScheme.SupportsValidateIdentity
 ENV_PROP_FILE=/solution/$CONFIG/data/environment.properties
 OBJECT_FILE=/solution/$CONFIG/object/SMPS_Objects.xml
 
+STORE_AVAILABILITY_TIME_OUT=500
+STORE_INITIALIZATION_TIME_OUT=600
+
+echo "[*][$(date +"%T")] - Create default objects: starting"
 ps_home=`/opt/util/parser.sh nete_ps_root $ENV_PROP_FILE`
 shared_key=`/opt/util/parser.sh nete_shared_key $ENV_PROP_FILE`
 ldap_ip=`/opt/util/parser.sh POLICY_STORE_HOST $ENV_PROP_FILE`
@@ -16,6 +25,9 @@ ldap_ssl=`/opt/util/parser.sh POLICY_STORE_SSL_ENABLED $ENV_PROP_FILE`
 ldap_fips_mode=`/opt/util/parser.sh nete_fips_mode $ENV_PROP_FILE`
 su_password=`/opt/util/parser.sh nete_su_password $ENV_PROP_FILE`
 
+POLICY_STORE_INITIALIZATION_COMPLETE_INDICATOR="ou=SSOInitialized"
+POLICY_STORE_INITIALIZATION_COMPLETE_INDICATOR_DN="$POLICY_STORE_INITIALIZATION_COMPLETE_INDICATOR, $ldap_rootdn"
+
 #decrypted using ../password-util/passwordDecode.sh
 su_password=`/solution/$CONFIG/../common/password-util/passwordDecode.sh $su_password`
 shared_key=`/solution/$CONFIG/../common/password-util/passwordDecode.sh $shared_key`
@@ -23,93 +35,145 @@ ldap_password=`/solution/$CONFIG/../common/password-util/passwordDecode.sh $ldap
 
 set CA_SM_PS_FIPS140=$ldap_fips_mode
 
+##### Functions
+ 
+wait_for_policy_store_to_start()
+{
+    i=0
+    # Waiting for the Policy Store service to be ready
+    while true; do
+        if [ "$i" -gt "$STORE_AVAILABILITY_TIME_OUT" ]; then
+            echo "[*][$(date +"%T")] - A timeout was reached ($STORE_AVAILABILITY_TIME_OUT seconds) while waiting for the policy store to be available."
+            exit 1
+        fi
+
+        $NETE_PS_ROOT/bin/smldapsetup status | grep -q "Error"
+        if [ $? -ne 0 ]; then
+            echo "[*][$(date +"%T")] - Policy store is up and running."
+            retval=0
+            break
+        else
+            echo "[*][$(date +"%T")] - Unable to connect to the policy store. Waiting for the policy-store to be available ($i).........."
+            i=`expr $i + 10`
+            sleep 10
+        fi
+    done
+} 
+
+initialize_policy_store()
+{ 
+    echo "[*][$(date +"%T")] - Initializing policy store:"
+    echo "[*][$(date +"%T")] - Setting SiteMinder's super user password."
+
+    /opt/CA/tmp/smreg -su $su_password
+
+    #echo "[*][$(date +"%T")] - Finished set super user password"
+    echo "[*][$(date +"%T")] - Starting XPSDDinstall"
+
+    $NETE_PS_ROOT/bin/XPSDDInstall $NETE_PS_ROOT/xps/dd/SmMaster.xdd
+
+    echo "[*][$(date +"%T")] - FinishedXPSDDInstall"
+
+    echo "[*][$(date +"%T")] - Starting import default objects"
+    
+    $NETE_PS_ROOT/bin/XPSImport $NETE_PS_ROOT/db/smpolicy.xml -npass
+    $NETE_PS_ROOT/bin/XPSImport $NETE_PS_ROOT/db/ampolicy.xml -npass
+    $NETE_PS_ROOT/bin/XPSImport $NETE_PS_ROOT/db/fedpolicy-12.5.xml -npass
+    $NETE_PS_ROOT/bin/XPSImport $OBJECT_FILE -npass
+    $NETE_PS_ROOT/bin/XPSImport /solution/$CONFIG/object/proxyui_objects.xml -npass
+
+    echo "[*][$(date +"%T")] - Finished import default objects"
+
+    echo "[*][$(date +"%T")] - Starting XPS Reg client"
+
+    $NETE_PS_ROOT/bin/XPSRegClient siteminder:$su_password -adminui-setup -t 1440
+
+    echo "[*][$(date +"%T")] - Finished XPS Reg client..."
+    
+    #Creating an indicator object in the store to mark the completion of store initialization
+    echo version: 1 >SSOInitialized.ldif
+    echo dn: $POLICY_STORE_INITIALIZATION_COMPLETE_INDICATOR_DN >>SSOInitialized.ldif
+    echo objectClass: organizationalUnit >>SSOInitialized.ldif
+    echo objectClass: top >>SSOInitialized.ldif
+    echo ou: SSOInitialized >>SSOInitialized.ldif
+    echo description: SSO version $POLICY_STORE_VERSION >>SSOInitialized.ldif
+    smldapmodify  -h $ldap_ip -p $ldap_port -D $ldap_userdn -w $ldap_password -a -f SSOInitialized.ldif
+    echo "[*][$(date +"%T")] - Policy Store initialization completed"
+}
+
+wait_for_policy_store_to_be_initialized()
+{
+    i=0
+    echo "[*][$(date +"%T")] - Waiting for the Policy Store to be initialized by the Master Policy Server"
+    while true; do
+        if [ "$i" -gt "$STORE_INITIALIZATION_TIME_OUT" ]; then
+        echo "[*][$(date +"%T")] - A timeout was reached ($STORE_AVAILABILITY_TIME_OUT seconds) while waiting for the policy store to be initialized."
+        exit 1
+        fi
+        ldapsearch -b "$POLICY_STORE_INITIALIZATION_COMPLETE_INDICATOR_DN" -h $ldap_ip -p $ldap_port -D $ldap_userdn -w $ldap_password -R -1 -s base "objectClass=*" description 2>/dev/null
+        if [ $? -eq 0 ]; then
+            echo "[*][$(date +"%T")] - Policy store is initialized and ready."
+            retval=0
+            break
+        else
+            echo "[*][$(date +"%T")] - Waiting for the policy store to be initialized ($i)...."
+            i=`expr $i + 10`
+            sleep 10
+        fi
+    done
+ }
+ 
+ ##### Main
 source $NETE_PS_ROOT/ca_ps_env.ksh
 
-echo "[*] Starting smreg operations"
-
+echo "[*][$(date +"%T")] - Starting smreg operations"
 /opt/CA/tmp/smreg LoadRegKeys "$ps_home" "" "EN"
 /opt/CA/tmp/smreg TestCryptoConfig "$shared_key"  "0" "" "" ""
 /opt/CA/tmp/smreg SetCryptoConfig "$shared_key"  "0" "" "" ""
 /opt/CA/tmp/smreg $DASH_PIN$ LoadInstallKey -123
 /opt/CA/tmp/smreg -key $shared_key
+echo "[*][$(date +"%T")] - Finished executing smreg"
 
-echo "[*] Finished executing smreg"
-
-echo "[*] Starting smldapsetup"
-
-echo "NETE_PS_ROOT=$NETE_PS_ROOT"
-echo "CAPKIHOME=$CAPKIHOME"
-echo "POLICY_STORE_HOST=$ldap_ip"
-echo "POLICY_STORE_PORT=$ldap_port"
-echo "POLICY_STORE_USER_DN=$ldap_userdn"
-echo "POLICY_STORE_ROOT_DN=$ldap_rootdn"
+echo "[*][$(date +"%T")] - Configure Policy Server to point at the policy store."
+cat << _EOM_
+    NETE_PS_ROOT=$NETE_PS_ROOT
+    CAPKIHOME=$CAPKIHOME
+    POLICY_STORE_HOST=$ldap_ip
+    POLICY_STORE_PORT=$ldap_port
+    POLICY_STORE_USER_DN=$ldap_userdn
+    POLICY_STORE_ROOT_DN=$ldap_rootdn
+_EOM_
 
 $NETE_PS_ROOT/bin/smldapsetup switch
-
 $NETE_PS_ROOT/bin/smldapsetup reg -h$ldap_ip -p$ldap_port -d$ldap_userdn -w$ldap_password -r$ldap_rootdn -ssl$ldap_ssl
 
-echo "[*] Finished smldapsetup"
+echo "[*][$(date +"%T")] - Checking if the policy store is up and running..."
+wait_for_policy_store_to_start
 
-TIME_OUT=500
-i=0
-
-while true; do
-    if [ "$i" -gt "$TIME_OUT" ]; then
-        exit 1
-    fi
-
-    $NETE_PS_ROOT/bin/smldapsetup status | grep -q "Error"
+echo "[*][$(date +"%T")] - Policy Server is running as $ROLE."
+if [ "$ROLE" == "master" ]; then
+    #Checking whether the scheme is of the right version by checking the existence of a certain attribute
+    echo r> replay.cmd
+    echo $POLICY_STORE_VERIFICATION_ATTRIBUTE>> replay.cmd
+    $NETE_PS_ROOT/bin/XPSDictionary <replay.cmd 2>/dev/null | grep "matched"
     if [ $? -ne 0 ]; then
-        echo "Policy store is ready..."
-        retval=0
-        break
+        echo "[*][$(date +"%T")] - Policy store is not initialized or does not run an updated schema."
+        initialize_policy_store
     else
-        echo "Unable to contact policy store...."
-        i=`expr $i + 10`
-        echo "Sleeping for 10 secs and re trying.........."
-        sleep 10
+        echo "[*][$(date +"%T")] - Policy store is initialized and running with the latest schema"
     fi
-done
+else #Worker Policy Server will wait for the Policy Store to be initialized by the "Master Policy Server"
+    wait_for_policy_store_to_be_initialized
+    echo SM > replay.cmd
+    echo CA.SM::\$EnableKeyGeneration >> replay.cmd
+    
+    $NETE_PS_ROOT/bin/XPSConfig <replay.cmd 2>/dev/null |grep -A 20 -F "PARAMETER MENU***********************************CA.SM::$EnableKeyGeneration" |grep "Current Value:  [ \t]* \"TRUE\""
+    if [ $? -eq 0 ]; then
+        echo "[*][$(date +"%T")] - Configuring Policy Server not to generate agent keys."
+        echo C >>replay.cmd
+        $NETE_PS_ROOT/bin/XPSConfig <replay.cmd 2>/dev/null |grep -A 20 -F "PARAMETER MENU***********************************CA.SM::$EnableKeyGeneration" |grep "Pending Value:  [ \t]* \"FALSE\""
+    else
+      echo "[*][$(date +"%T")] - Policy Server is set not to generate agent keys."
+    fi
+fi
 
-echo "[*] Starting set super user password"
-/opt/CA/tmp/smreg -su $su_password
-
-echo "[*] Finished set super user password"
-
-echo CA.SM::AuthScheme> replay.cmd
-
-echo A>> replay.cmd
-
-XPSExplorer < replay.cmd 2>/dev/null | grep SupportsValidateIdentity
-    if [ $? -ne 0 ]; then
-		echo "[*] Starting XPSDDinstall"
-		# TODO: Do not import schema if it is already there
-
-		$NETE_PS_ROOT/bin/XPSDDInstall $NETE_PS_ROOT/xps/dd/SmMaster.xdd
-
-		echo "[*] FinishedXPSDDInstall"
-
-		echo "[*] Starting import default objects"
-		# TODO: Do not import data if it is already there
-
-		$NETE_PS_ROOT/bin/XPSImport $NETE_PS_ROOT/db/smpolicy.xml -npass
-
-		$NETE_PS_ROOT/bin/XPSImport $NETE_PS_ROOT/db/ampolicy.xml -npass
-
-		$NETE_PS_ROOT/bin/XPSImport $NETE_PS_ROOT/db/fedpolicy-12.5.xml -npass
-
-		$NETE_PS_ROOT/bin/XPSImport $OBJECT_FILE -npass
-
-		$NETE_PS_ROOT/bin/XPSImport /solution/$CONFIG/object/proxyui_objects.xml -npass
-
-		echo "[*] Finished import default objects"
-	fi
-
-
-echo "[*] Starting XPS Reg client"
-
-$NETE_PS_ROOT/bin/XPSRegClient siteminder:$su_password -adminui-setup -t 1440
-
-echo "[*] Finished XPS Reg client..."
-
-echo "[*] Create default objects: complete"
